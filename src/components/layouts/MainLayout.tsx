724228d0444d0b01aa20155dcd0eaa2a8b01fbf7
Rabeya Akter <rabeyariya251@gmail.com>
2025-01-22 15:21:22 +0600

    set route

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,25 +1,15 @@
 import { Layout, Menu } from 'antd';
+import { router } from '../../routes/router';
+import { Link, Outlet } from 'react-router-dom';
 const { Header, Content, Sider } = Layout;
-const MainLayout = () => {
-    const items = [
-        {
-            label: 'Profile',
-            key: 'profile',
-        },
-        {
-            label: 'Analytics',
-            key: 'analytics',
-        },
-        {
-            label: 'Donors',
-            key: 'donors',
-        },
-        {
-            label: 'Deposits',
-            key: 'deposits',
-        },
 
-    ]
+const MainLayout = () => {
+    const rootRoute = router.routes.find((route) => route.path === '/');
+
+    const menuItems = rootRoute?.children?.map((child) => ({
+        key: `/${child.path}`,
+        label: <Link to={`/${child.path}`}>{`${child.path}`.replace("/", "").toUpperCase()}</Link>,
+    }));
 
     return (
         <Layout>
@@ -33,33 +23,27 @@ const MainLayout = () => {
                     left: 0,
                 }}
             >
-                <div style={{
-                    color: 'white',
-                    height: '4rem',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }} className="demo-logo-vertical" >Cresent</div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+                <div
+                    style={{
+                        color: 'white',
+                        height: '4rem',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                    className="demo-logo-vertical"
+                >
+                    Cresent
+                </div>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={menuItems} />
             </Sider>
-            <Layout>
-                <Header style={{ padding: 0, }} />
-                <Content style={{
-                    margin: '24px 16px',
-                    padding: 24,
-                    height: '100vh',
-                }}>
-                    <div
-                        style={{
-                            padding: 24,
-                            minHeight: 360,
-
-                        }}
-                    >
-                        content
+            <Layout style={{ marginLeft: 200 }}>
+                <Header style={{ padding: 0 }} />
+                <Content style={{ margin: '24px 16px', padding: 24, height: '100vh' }}>
+                    <div style={{ padding: 24, minHeight: 360 }}>
+                        <Outlet />
                     </div>
                 </Content>
-
             </Layout>
         </Layout>
     );
