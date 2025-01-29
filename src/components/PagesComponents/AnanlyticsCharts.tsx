9e173cefe27812cc98f51f9f3698ad697ad930fa
Rabeya Akter <rabeyariya251@gmail.com>
2025-01-29 11:45:47 +0600

    complete pie chart

--- src/components/PagesComponents/AnanlyticsCharts.tsx
+++ src/components/PagesComponents/AnanlyticsCharts.tsx
@@ -1,12 +1,27 @@
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip } from "recharts";
+import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip, PieChart, Pie, Cell } from "recharts";
 
-interface ChartData {
+interface BarChartData {
     month: string;
     transactions: number;
 }
 
+interface PieChartData {
+    name: string;
+    value: number;
+    color: string;
+
+}
+interface renderCustomizedLabelProps {
+    cx: number;
+    cy: number;
+    midAngle: number;
+    innerRadius: number;
+    outerRadius: number;
+    percent: number;
+}
+
 const AnalyticsCharts: React.FC = () => {
-    const data: ChartData[] = [
+    const barData: BarChartData[] = [
         { month: "January", transactions: 4000 },
         { month: "February", transactions: 3000 },
         { month: "March", transactions: 2000 },
@@ -21,19 +36,73 @@ const AnalyticsCharts: React.FC = () => {
         { month: "December", transactions: 1890 },
     ];
 
+    const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042"];
+
+    const pieData: PieChartData[] = [
+        { name: "General", value: 5, color: COLORS[0] },
+        { name: "Utilities", value: 5, color: COLORS[1] },
+        { name: "Women", value: 5, color: COLORS[2] },
+        { name: "Imam", value: 5, color: COLORS[3] },
+    ];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel: React.FC<renderCustomizedLabelProps> = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+                <div className="flex flex-c">
+
+                </div>
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
     return (
-        <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
-                <div className="border p-4 rounded-md">
-                    <p className="font-bold text-lg mb-4">Monthly Transactions</p>
-                    <ResponsiveContainer width="100%" height={300}>
-                        <BarChart data={data}>
-                            <XAxis dataKey="month" stroke="#8884d8" />
-                            <YAxis />
-                            <Tooltip />
-                            <Bar dataKey="transactions" fill="#8884d8" />
-                        </BarChart>
-                    </ResponsiveContainer>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10">
+            <div className="border p-4 rounded-md">
+                <p className="font-bold text-lg mb-4">Monthly Transactions</p>
+                <ResponsiveContainer width="100%" height={300}>
+                    <BarChart data={barData}>
+                        <XAxis dataKey="month" stroke="#8884d8" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="transactions" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+
+            <div className="border p-4 rounded-md">
+                <p className="font-bold text-lg mb-4">Envelope Types</p>
+                <ResponsiveContainer width="100%" height={300}>
+                    <PieChart>
+                        <Pie
+                            data={pieData}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {pieData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={entry.color} />
+                            ))}
+                        </Pie>
+                        <Tooltip />
+                    </PieChart>
+                </ResponsiveContainer>
+                <div className="mt-4 flex justify-center items-center gap-5">
+                    {pieData.map((item, index) => (
+                        <div key={index} className="flex items-center mb-2">
+                            <div className="w-4 h-4 rounded mr-2" style={{ backgroundColor: item.color }}></div>
+                            <span>{item.name}</span>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
