c0c0ea9095c691822d1b3dd8996ab2398fe33bd8
Rabeya Akter <rabeyariya251@gmail.com>
2025-01-29 13:00:32 +0600

    complete invoices

--- src/components/PagesComponents/AvailableSubscriptions.tsx
+++ src/components/PagesComponents/AvailableSubscriptions.tsx
@@ -45,24 +45,29 @@ const AvailableSubscriptions = () => {
     ];
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            {subscriptions.map((sub, index) => (
-                <div key={index} className="p-4 border border-primary rounded-md shadow-md">
-                    <h3 className="text-lg font-semibold">{sub.title}</h3>
-                    <p className="flex items-center gap-1 text-xl font-bold text-primary">
-                        <FaDollarSign /> {sub.price}
-                    </p>
-                    <p className="text-sm text-gray-600">{sub.target}</p>
-                    <ul className="mt-2 text-sm">
-                        {sub.features.map((feature, i) => (
-                            <li key={i} className="flex items-center gap-1">
-                                ✅ {feature}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
+        <div className="mt-10">
+            <h1 className="text-3xl font-bold my-5">Available Subscriptions Plan</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+
+                {subscriptions.map((sub, index) => (
+                    <div key={index} className="p-4 border border-primary rounded-md shadow-md">
+                        <h3 className="text-lg font-semibold">{sub.title}</h3>
+                        <p className="flex items-center gap-1 text-xl font-bold text-primary">
+                            <FaDollarSign /> {sub.price}
+                        </p>
+                        <p className="text-sm text-gray-600">{sub.target}</p>
+                        <ul className="mt-2 text-sm">
+                            {sub.features.map((feature, i) => (
+                                <li key={i} className="flex items-center gap-1">
+                                    ✅ {feature}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
         </div>
+
     );
 };
 
--- src/pages/Subscriptions/Subscriptions.tsx
+++ src/pages/Subscriptions/Subscriptions.tsx
@@ -7,6 +7,7 @@ const Subscriptions = () => {
         <div>
             <h1 className="text-3xl font-bold">Subscriptions</h1>
             <SubscriptionCard />
+           
             <AvailableSubscriptions />
             <Invoices />
         </div>
