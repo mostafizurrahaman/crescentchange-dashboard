50c137743b93d9bdb26a0cb6f206d5233c58923e
Rabeya Akter <rabeyariya251@gmail.com>
2025-05-26 09:09:19 +0600

    subscription

--- src/pages/Subscriptions/Subscriptions.tsx
+++ src/pages/Subscriptions/Subscriptions.tsx
@@ -3,15 +3,22 @@ import Invoices from "../../components/PagesComponents/Invoices";
 import SubscriptionCard from "../../components/PagesComponents/SubscriptionCard";
 
 const Subscriptions = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold">Subscriptions</h1>
-            <SubscriptionCard />
-           
-            <AvailableSubscriptions />
-            <Invoices />
-        </div>
-    );
+  return (
+    <div>
+      <div className="w-full">
+        <h1 className="text-xl md:text-3xl font-semibold my-3">
+          Subscriptions
+        </h1>
+        <p className="text-gray-500 mb-10">
+          Overview of your active subscriptions.
+        </p>
+      </div>
+      <SubscriptionCard />
+
+      <AvailableSubscriptions />
+      <Invoices />
+    </div>
+  );
 };
 
 export default Subscriptions;
