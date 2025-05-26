015f725ee65b99262195b13021c03d6bee60b724
Rabeya Akter <rabeyariya251@gmail.com>
2025-05-26 08:01:55 +0600

    implement desposit

--- src/pages/Deposits/Deposits.tsx
+++ src/pages/Deposits/Deposits.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {  Select } from "antd";
+import { DatePicker, Select } from "antd";
 import { FC } from "react";
-import { FaHandHoldingHeart } from "react-icons/fa";
-import { FaUserGroup } from "react-icons/fa6";
+import card from "../../assets/images/card.png";
 import dollor from "../../assets/images/card.png";
 import img1 from "../../assets/images/img1.png";
 import img2 from "../../assets/images/img2.png";
-import img3 from "../../assets/images/Vector (2).png"
-import img4 from "../../assets/images/Vector (3).png"
+import img3 from "../../assets/images/Vector (2).png";
+import img4 from "../../assets/images/Vector (3).png";
 interface DepositData {
   key: string;
   date: string;
@@ -18,34 +17,6 @@ interface DepositData {
 }
 
 const Deposits: FC = () => {
-  const columns = [
-    {
-      title: "Date",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Donor Name",
-      dataIndex: "donor",
-      key: "donor",
-    },
-    {
-      title: "Amount",
-      dataIndex: "amount",
-      key: "amount",
-    },
-    {
-      title: "Payment Method",
-      dataIndex: "method",
-      key: "method",
-    },
-    {
-      title: "Card Number",
-      dataIndex: "card",
-      key: "card",
-    },
-  ];
-
   const data: DepositData[] = [
     {
       key: "1",
@@ -76,25 +47,26 @@ const Deposits: FC = () => {
 
   return (
     <div className="">
-      <div className="w-full md:w-[70%]">
+      <div className="w-full">
         <h1 className="text-xl md:text-3xl font-semibold my-3">Deposits</h1>
-        <p className="text-gray-500 mb-10">
-          Your funds, flowing with purpose. Track every deposit here.
-        </p>
+        <div className="flex justify-between items-center mb-5">
+          <p className="text-gray-500 mb-10">
+            Your funds, flowing with purpose. Track every deposit here.
+          </p>
+          <div>
+            <Select defaultValue="This Month" className="w-[150px]">
+              <Select.Option value="Last 30 Days">Last 30 Days</Select.Option>
+              <Select.Option value="2">Last 60 Days</Select.Option>
+              <Select.Option value="3">Last 90 Days</Select.Option>
+              <Select.Option value="4">Last 120 Days</Select.Option>
+            </Select>
+          </div>
+        </div>
       </div>
       <div>
-        <div className="flex justify-start items-center gap-5 my-5">
-          <h1 className="text-3xl font-bold">Analytics</h1>
-          <Select defaultValue="This Month" className="w-[150px]">
-            <Select.Option value="Last 30 Days">Last 30 Days</Select.Option>
-            <Select.Option value="2">Last 60 Days</Select.Option>
-            <Select.Option value="3">Last 90 Days</Select.Option>
-            <Select.Option value="4">Last 120 Days</Select.Option>
-          </Select>
-        </div>
         <div className="bg-[#fdb1f1] p-5 rounded-md relative ">
           <img src={dollor} alt="" className="mb-5" />
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="flex gap-6 justify-between items-center">
             <div>
               <div className="flex justify-start items-center gap-5">
                 <h1 className="text-3xl md:text-5xl font-bold"> $40,000</h1>
@@ -105,16 +77,15 @@ const Deposits: FC = () => {
               </div>
               <p className="text-gray-500 text-2xl mt-5">Total Donated</p>
             </div>
-            <div className="grid grid-cols-2 gap-6">
-              <div className="bg-white p-5 rounded-md">
-                <FaHandHoldingHeart className="h-8 w-8" />
-                <h1 className="text-2xl font-bold my-3">$400</h1>
-                <p>Avg Donation</p>
-              </div>
-              <div className="bg-white p-5 rounded-md">
-                <FaUserGroup className="h-8 w-8" />
-                <h1 className="text-2xl font-bold my-3">$400</h1>
-                <p>Total donors</p>
+            <div className="flex flex-col gap-6 absolute right-5 top-10 z-[1]">
+              <DatePicker
+                className="w-full"
+                placeholder="Select Deposit Date"
+                style={{ width: "200px" }}
+              ></DatePicker>
+              <div className="flex justify-between items-center bg-white px-2 rounded-md mt-10">
+                <img src={card} alt="" />
+                Your Next Payout is onits way -May 10
               </div>
             </div>
           </div>
