7aa8adb50a30b4ddb3ff5d41608d4978fe1829fc
Rabeya Akter <rabeyariya251@gmail.com>
2025-05-29 09:18:48 +0600

    implement intregation

--- src/components/SettingsComponents/Intregation.tsx
+++ src/components/SettingsComponents/Intregation.tsx
@@ -1,10 +1,89 @@
+import img1 from "../../assets/images/hubspot logo 1.png";
+import img2 from "../../assets/images/Monday 1.png";
+import img3 from "../../assets/images/MC_Logo_0 1.png";
+import img4 from "../../assets/images/Microsoft_Outlook-Logo.wine 1.png";
+import { Switch } from "antd";
 
 const Intregation = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <h1 className="text-3xl font-bold my-10">Intregation</h1>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img1} alt="" />
+          <h1 className="text-2xl font-bold my-3">Hubspot</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
         </div>
-    );
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img2} alt="" />
+          <h1 className="text-2xl font-bold my-3">Monday</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img3} alt="" />
+          <h1 className="text-2xl font-bold my-3">Mail Chimp</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img4} alt="" />
+          <h1 className="text-2xl font-bold my-3">Hubspot</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img1} alt="" />
+          <h1 className="text-2xl font-bold my-3">Hubspot</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img2} alt="" />
+          <h1 className="text-2xl font-bold my-3">Monday</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img3} alt="" />
+          <h1 className="text-2xl font-bold my-3">Mail Chimp</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img4} alt="" />
+          <h1 className="text-2xl font-bold my-3">Hubspot</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img1} alt="" />
+          <h1 className="text-2xl font-bold my-3">Hubspot</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img2} alt="" />
+          <h1 className="text-2xl font-bold my-3">Monday</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img3} alt="" />
+          <h1 className="text-2xl font-bold my-3">Mail Chimp</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+        <div className="border border-black rounded-2xl p-5">
+          <img src={img4} alt="" />
+          <h1 className="text-2xl font-bold my-3">Hubspot</h1>
+          <p className="my-5">Enable/disable email or SMS notifications to users.</p>
+          <Switch></Switch>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Intregation;
