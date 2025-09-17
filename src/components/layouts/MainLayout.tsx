a2a0c2450db225d9a0d4a40e39264434b6453809
Rabeya Akter <rabeyariya251@gmail.com>
2025-09-17 16:30:02 +0600

    chnage layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,40 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { Outlet } from "react-router-dom";
-import { useState } from 'react';
-import { Layout } from 'antd';
+import { useState } from "react";
+import { Layout } from "antd";
 import Sidebar from "./Sidebar/Sidebar";
-// import MainHeader from "./MainHeader/MainHeader";
-
+import MainHeader from "./MainHeader/MainHeader";
 
 const { Content } = Layout;
 const MainLayout = () => {
-    const [collapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
 
+  return (
+    <div className="">
+      <Layout>
+        <Sidebar collapsed={collapsed}></Sidebar>
+        <Layout
+          style={{
+            marginLeft: collapsed ? 80 : 250,
+            transition: "margin-left 0.2s ease",
+          }}
+          className={``}
+        >
+          {/* my header */}
+          <MainHeader
+            setCollapsed={setCollapsed}
+            collapsed={collapsed}
+          ></MainHeader>
 
-    return (
-        <div className="">
-            <Layout >
-                <Sidebar collapsed={collapsed} ></Sidebar>
-                <Layout
-                    style={{
-                        marginLeft: collapsed ? 80 : 250,
-                        transition: 'margin-left 0.2s ease',
-                    }}
-                    className={``}>
-                    {/* my header */}
-                    {/* <MainHeader setCollapsed={setCollapsed} collapsed={collapsed}></MainHeader> */}
-                    <Content
-                        className="p-5 bg-white"
-                        style={{}}
-                    >
-                        {/* my content */}
-                        <Outlet></Outlet>
-                    </Content>
-                </Layout>
-            </Layout>
-        </div>
-    );
+          <Content className="p-5 bg-white" style={{}}>
+            {/* my content */}
+            <Outlet></Outlet>
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
+  );
 };
 
 export default MainLayout;
