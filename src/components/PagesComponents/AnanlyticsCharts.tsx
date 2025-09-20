c1bb249b91c56f8d087bff2fcc4cbe0ce1144f0d
Rabeya Akter <rabeyariya251@gmail.com>
2025-09-20 10:19:41 +0600

    analytics charts

--- src/components/PagesComponents/AnanlyticsCharts.tsx
+++ src/components/PagesComponents/AnanlyticsCharts.tsx
@@ -1,54 +1,68 @@
-import { FaArrowDown, FaArrowUp } from "react-icons/fa";
-import db from "../../assets/images/db.png";
-import flower3 from "../../assets/images/Vector (1).png";
-import flower4 from "../../assets/images/Vector.png";
-import flower5 from "../../assets/images/Frame.png";
-import recurring from "../../assets/images/recurring.png";
-import onetime from "../../assets/images/ontime.png";
 interface AnalyticsCardProps {
   filter: "Today" | "This Week" | "This Month";
   data: { message: string };
 }
 
-const AnanlyticsCharts:React.FC<AnalyticsCardProps> = () => {
+import oneTime from "../../assets/images/one-time.png";
+import recurring from "../../assets/images/recurring.png";
+import rounup from "../../assets/images/roundup.png";
+
+const AnanlyticsCharts: React.FC<AnalyticsCardProps> = () => {
   return (
-    <div className=" my-5">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-        <div className="bg-[#b5e0ff] p-5 rounded-2xl relative">
-          <img src={db} alt="" />
-          <p className="text-gray-500 my-5 text-xl">Round Up</p>
-          <div className="flex justify-start items-center gap-5">
-            <h1 className="text-3xl font-bold">$40,000</h1>
-            <p className="flex justify-center items-center gap-2 text-green-500 bg-white px-4 py-1 rounded-2xl">
-              <FaArrowUp />
-              8.2%
+    <div className=" my-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+        <div className="bg-white border p-6 rounded-2xl relative">
+          <div className="flex justify-between items-start mb-6 h-20">
+            <div>
+              <p className=" text-xl font-medium">Round Up</p>
+              <p className="text-gray-500">
+                Donations from automatic spare change
+              </p>
+            </div>
+            <img src={rounup} alt="" />
+          </div>
+          <div className="">
+            <h1 className="text-3xl font-bold">
+              <span className="text-gray-400">$</span>40,000
+            </h1>
+            <p className="text-gray-400 mt-2">
+              <span className="text-green-500"> +8.2% </span>vs last month
             </p>
           </div>
-          <img src={flower3} alt="" className="absolute bottom-0 right-0 top-0"/>
         </div>
-        <div className="bg-[#9df2c1] p-5 rounded-2xl relative">
-          <img src={recurring} alt="" />
-          <p className="text-gray-500 my-5 text-xl">Recurring</p>
-          <div className="flex justify-start items-center gap-5">
-            <h1 className="text-3xl font-bold">$40,000</h1>
-            <p className="flex justify-center items-center gap-2 text-red-500 bg-white px-4 py-1 rounded-2xl">
-              <FaArrowDown />
-              8.2%
+        <div className="bg-white border p-6 rounded-2xl relative">
+          <div className="flex justify-between items-start  mb-6 h-20">
+            <div>
+              <p className=" text-xl font-medium">Recurring</p>
+              <p className="text-gray-500">Commitments</p>
+            </div>
+            <img src={recurring} alt="" />
+          </div>
+          <div className="">
+            <h1 className="text-3xl font-bold">
+              <span className="text-gray-400">$</span>40,000
+            </h1>
+            <p className="text-gray-400 mt-2">
+              <span className="text-green-500"> +8.2% </span>vs last month
             </p>
           </div>
-          <img src={flower4} alt="" className="absolute bottom-0 right-0 top-0"/>
         </div>
-        <div className="bg-[#ffe599] p-5 rounded-2xl relative">
-          <img src={onetime} alt="" />
-          <p className="text-gray-500 my-5 text-xl">One-Time</p>
-          <div className="flex justify-start items-center gap-5">
-            <h1 className="text-3xl font-bold">$40,000</h1>
-            <p className="flex justify-center items-center gap-2 text-green-500 bg-white px-4 py-1 rounded-2xl">
-              <FaArrowUp />
-              8.2%
+        <div className="bg-white border p-6 rounded-2xl relative">
+          <div className="flex justify-between items-start  mb-6 h-20">
+            <div>
+              <p className=" text-xl font-medium">One-time</p>
+              <p className="text-gray-500">Single contributions</p>
+            </div>
+            <img src={oneTime} alt="" />
+          </div>
+          <div className="">
+            <h1 className="text-3xl font-bold">
+              <span className="text-gray-400">$</span>40,000
+            </h1>
+            <p className="text-gray-400 mt-2">
+              <span className="text-green-500"> +8.2% </span>vs last month
             </p>
           </div>
-          <img src={flower5} alt="" className="absolute bottom-0 right-0 top-0"/>
         </div>
       </div>
     </div>
