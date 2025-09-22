dc83f789aa192a5889175d70188fd1f2bccb9c80
RabeyaAkter78 <rabeyariya251@gmail.com>
2025-09-22 16:21:28 +0600

    implement integration

--- src/pages/Integration/Integration.tsx
+++ src/pages/Integration/Integration.tsx
@@ -1,10 +1,20 @@
-
 const Integration = () => {
-    return (
+  return (
+    <div>
+      <div className="flex justify-between items-center gap-5">
         <div>
-            
+          <h1 className="text-3xl font-bold mb-4">Integrations</h1>
+          <p className="text-lg text-gray-600 mb-4">
+            Each name represents a journey of support. Watch them glow.
+          </p>
         </div>
-    );
+      </div>
+
+      <div className="my-6 grid grid-cols-1 md:grid-cols-3 gap-3 justify-between items-center">
+        <div className="bg-white p-6 border rounded-3xl"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Integration;
--- src/pages/Reports/Reports.tsx
+++ src/pages/Reports/Reports.tsx
@@ -126,82 +126,80 @@ const Reports = () => {
         </div>
       </div>
 
-      {activeTab === "All Donors" && (
-        <div>
-          <div className="grid grid-cols-1 md:grid-cols-3 justify-center  items-center gap-3">
-            <div className="bg-white p-6 rounded-3xl border">
-              <p className="text-lg font-medium">Total Donations</p>
-              <h1 className="text-2xl font-medium mt-10">
-                {" "}
-                <span className="text-gray-400">$</span> 4000{" "}
-                <span className="text-sm text-green-400">+8.2% </span>{" "}
-                <span className="text-gray-400 text-sm">vs last month</span>{" "}
-              </h1>
-            </div>
-            <div className="bg-white p-6 rounded-3xl border">
-              <p className="text-lg font-medium">Total Donors</p>
-              <h1 className="text-2xl font-medium mt-10">
-                {" "}
-                <span className="text-gray-400">$</span> 4000{" "}
-                <span className="text-sm text-green-500">5.4%</span>{" "}
-              </h1>
-            </div>
-            <div className="bg-white p-6 rounded-3xl border">
-              <p className="text-lg font-medium">Avg. Donation</p>
-              <h1 className="text-2xl font-medium mt-10">
-                {" "}
-                <span className="text-gray-400">$</span> 400{" "}
-                <span className="text-sm text-gray-400">per user</span>{" "}
-              </h1>
-            </div>
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-center  items-center gap-3">
+          <div className="bg-white p-6 rounded-3xl border">
+            <p className="text-lg font-medium">Total Donations</p>
+            <h1 className="text-2xl font-medium mt-10">
+              {" "}
+              <span className="text-gray-400">$</span> 4000{" "}
+              <span className="text-sm text-green-400">+8.2% </span>{" "}
+              <span className="text-gray-400 text-sm">vs last month</span>{" "}
+            </h1>
           </div>
-          <div className="bg-white p-6 rounded-3xl border my-6">
-            <div className="flex justify-between items-center gap-5">
-              <h1 className="text-xl font-medium">Donation History</h1>
-
-              <div className="flex items-center gap-3">
-                <div className="mt-4 md:mt-0">
-                  <Search
-                    placeholder="input search text"
-                    onSearch={onSearch}
-                    enterButton
-                  />
-                </div>
-
-                <div className="mt-4 md:mt-0">
-                  <Select defaultValue="selected" style={{ width: 120 }}>
-                    <Option value="selected">Selected</Option>
-                    <Option value="all">All</Option>
-                  </Select>
-                </div>
-
-                <div className="mt-4 md:mt-0">
-                  <button className="px-3 py-2 border rounded-md text-sm text-gray-700">
-                    Monthly
-                  </button>
-                </div>
-                {/* export korte hobe */}
-                <div className="group relative mt-4 md:mt-0">
-                  <MoreOutlined className="text-xl cursor-pointer" />
-                  <span className="absolute left-0 bottom-0 text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-                    Export
-                  </span>
-                </div>
-              </div>
-            </div>
-            <Table
-              columns={columns}
-              dataSource={data}
-              pagination={{ pageSize: 5 }}
-              style={{ marginTop: 20 }}
-            />
-
-            <div className="flex justify-end items-center my-10">
-              <Pagination />
-            </div>
+          <div className="bg-white p-6 rounded-3xl border">
+            <p className="text-lg font-medium">Total Donors</p>
+            <h1 className="text-2xl font-medium mt-10">
+              {" "}
+              <span className="text-gray-400">$</span> 4000{" "}
+              <span className="text-sm text-green-500">5.4%</span>{" "}
+            </h1>
+          </div>
+          <div className="bg-white p-6 rounded-3xl border">
+            <p className="text-lg font-medium">Avg. Donation</p>
+            <h1 className="text-2xl font-medium mt-10">
+              {" "}
+              <span className="text-gray-400">$</span> 400{" "}
+              <span className="text-sm text-gray-400">per user</span>{" "}
+            </h1>
           </div>
         </div>
-      )}
+        <div className="bg-white p-6 rounded-3xl border my-6">
+          <div className="flex justify-between items-center gap-5">
+            <h1 className="text-xl font-medium">Donation History</h1>
+
+            <div className="flex items-center gap-3">
+              <div className="mt-4 md:mt-0">
+                <Search
+                  placeholder="input search text"
+                  onSearch={onSearch}
+                  enterButton
+                />
+              </div>
+
+              <div className="mt-4 md:mt-0">
+                <Select defaultValue="selected" style={{ width: 120 }}>
+                  <Option value="selected">Selected</Option>
+                  <Option value="all">All</Option>
+                </Select>
+              </div>
+
+              <div className="mt-4 md:mt-0">
+                <button className="px-3 py-2 border rounded-md text-sm text-gray-700">
+                  Monthly
+                </button>
+              </div>
+              {/* export korte hobe */}
+              <div className="group relative mt-4 md:mt-0">
+                <MoreOutlined className="text-xl cursor-pointer" />
+                <span className="absolute left-0 bottom-0 text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+                  Export
+                </span>
+              </div>
+            </div>
+          </div>
+          <Table
+            columns={columns}
+            dataSource={data}
+            pagination={{ pageSize: 5 }}
+            style={{ marginTop: 20 }}
+          />
+
+          <div className="flex justify-end items-center my-10">
+            <Pagination />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
