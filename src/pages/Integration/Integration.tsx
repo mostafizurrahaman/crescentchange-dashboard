4ba2f2c9c5f8fb9537ee986ba67709a3213c7852
RabeyaAkter78 <rabeyariya251@gmail.com>
2025-09-22 16:49:56 +0600

    complete integration part

--- src/pages/Integration/Integration.tsx
+++ src/pages/Integration/Integration.tsx
@@ -1,3 +1,10 @@
+import img1 from "../../assets/images/image 415.png";
+import img2 from "../../assets/images/slack.png";
+import img3 from "../../assets/images/image 417.png";
+import img4 from "../../assets/images/Logo (1).png";
+import { Switch } from "antd";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+
 const Integration = () => {
   return (
     <div>
@@ -11,7 +18,82 @@ const Integration = () => {
       </div>
 
       <div className="my-6 grid grid-cols-1 md:grid-cols-3 gap-3 justify-between items-center">
-        <div className="bg-white p-6 border rounded-3xl"></div>
+        <div className="bg-white p-6 border rounded-3xl">
+          <div className="flex justify-between items-center gap-3">
+            <img src={img1} alt="" />
+            <HiOutlineArrowNarrowRight className="h-5 w-5 " />
+          </div>
+          <div className="my-3 border-b pb-6">
+            <h1>Webflow</h1>
+            <p className="text-gray-400 mt-2">
+              Contributions from automatic spare change Support our cause
+              effortlessly!
+            </p>
+          </div>
+          <div className="flex justify-between items-center gap-3">
+            <button className="flex justify-center items-center gap-2">
+              Connect
+            </button>
+            <Switch></Switch>
+          </div>
+        </div>
+        <div className="bg-white p-6 border rounded-3xl">
+          <div className="flex justify-between items-center gap-3">
+            <img src={img2} alt="" />
+            <HiOutlineArrowNarrowRight className="h-5 w-5 " />
+          </div>
+          <div className="my-3 border-b pb-6">
+            <h1>Slack</h1>
+            <p className="text-gray-400 mt-2">
+              Contributions from automatic spare change Support our cause
+              effortlessly!
+            </p>
+          </div>
+          <div className="flex justify-between items-center gap-3">
+            <button className="flex justify-center items-center gap-2">
+              Connect
+            </button>
+            <Switch></Switch>
+          </div>
+        </div>
+        <div className="bg-white p-6 border rounded-3xl">
+          <div className="flex justify-between items-center gap-3">
+            <img src={img3} alt="" />
+            <HiOutlineArrowNarrowRight className="h-5 w-5 " />
+          </div>
+          <div className="my-3 border-b pb-6">
+            <h1>Google Sheets</h1>
+            <p className="text-gray-400 mt-2">
+              Contributions from automatic spare change Support our cause
+              effortlessly!
+            </p>
+          </div>
+          <div className="flex justify-between items-center gap-3">
+            <button className="flex justify-center items-center gap-2">
+              Connect
+            </button>
+            <Switch></Switch>
+          </div>
+        </div>
+        <div className="bg-white p-6 border rounded-3xl">
+          <div className="flex justify-between items-center gap-3">
+            <img src={img4} alt="" />
+            <HiOutlineArrowNarrowRight className="h-5 w-5 " />
+          </div>
+          <div className="my-3 border-b pb-6">
+            <h1>Hubspot</h1>
+            <p className="text-gray-400 mt-2">
+              Contributions from automatic spare change Support our cause
+              effortlessly!
+            </p>
+          </div>
+          <div className="flex justify-between items-center gap-3">
+            <button className="flex justify-center items-center gap-2">
+              Connect
+            </button>
+            <Switch></Switch>
+          </div>
+        </div>
       </div>
     </div>
   );
