ca2b3cfd5accd161f86bbc32d06d9c2fc80c2966
Rabeya Akter <rabeyariya251@gmail.com>
2025-12-13 16:31:11 +0600

    build and fix error

--- src/redux/utils/baseUrl.ts
+++ src/redux/utils/baseUrl.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://10.10.20.42:5000/api/v1";
+// export const BASE_URL = "http://10.10.20.42:5000/api/v1";
+export const BASE_URL = "https://intervention-initially-model-disclose.trycloudflare.com/api/v1";
 export const IMAGE_URL="http://10.10.20.42:5000/"
