3c7557165f8daa56d70956023fddb5adc9587676
Rabeya Akter <rabeyariya251@gmail.com>
2025-12-22 10:02:04 +0600

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  
-})
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "http://13.55.115.124:5000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1"),
+      },
+    },
+  },
+});
